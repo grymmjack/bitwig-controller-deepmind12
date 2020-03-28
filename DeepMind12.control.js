2a703b5c3b00263e0b4fe487cd7c7f82b10a92f0
Rick Christy <grymmz0r@yahoo.com>
2020-03-28 04:34:50 -0400

    Removed MS and OLD_MS - not necessary

--- DeepMind12.control.js
+++ DeepMind12.control.js
@@ -6,8 +6,6 @@ host.defineSysexIdentityReply("f0 7e ?? 06 01 00 20 32 20 00 01 00 ?? 00 ?? ?? F
 
 var SYSEX_HEADER = "F0 7E 7F 06 01 F7";
 
-var OLD_MS   = MS;
-var MS       = (new Date()).getMilliseconds();
 var NRPN_NUM = 0;
 var NRPN_VAL = 0;
 
@@ -24,6 +22,14 @@ var CHROMATIC = [ 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B
 var CC_IDX = [];
 var CC_MAP = [];
 var CC = {
+   UNISON_DETUNE        : 28,
+   VCF_FREQ             : 29,
+   VCF_RES              : 30,
+   VCF_ENV              : 31,
+   VCF_LFO              : 33,
+   VCF_KYBD             : 34,
+   VCA_LEVEL            : 36,
+   HPF_FREQ             : 35,
    ARP_RATE             : 12,
    ARP_GATE_TIME        : 13,
    LFO1_RATE            : 16,
@@ -37,14 +43,6 @@ var CC = {
    OSC2_PITCH           : 25,
    OSC2_LEVEL           : 26,
    NOISE_LEVEL          : 27,
-   UNISON_DETUNE        : 28,
-   VCF_FREQ             : 29,
-   VCF_RES              : 30,
-   VCF_ENV              : 31,
-   VCF_LFO              : 33,
-   VCF_KYBD             : 34,
-   VCA_LEVEL            : 36,
-   HPF_FREQ             : 35,
    ENV_VCA_A            : 37,
    ENV_VCA_D            : 39,
    ENV_VCA_S            : 40,
@@ -117,21 +115,21 @@ function init() {
 }
 
 function onMidi(status, data1, data2) {
-   MS = (new Date()).getMilliseconds();
    NRPN_NUM = (status == 176 && data1 == 98) ? data2 : NRPN_NUM;
    NRPN_VAL = (status == 176 && data1 == 38) ? data2 : NRPN_VAL;
    var NOTE_ON  = (status == 144);
    var NOTE_OFF = (status == 128);
    var NOTE_NUM = (NOTE_ON || NOTE_OFF) ? data1 : 0;
-   var NRPN_NEW = (MS != OLD_MS && status == 176 && data1 == 38 && NRPN_NUM);
+   var NRPN_NEW = (status == 176 && data1 == 38 && NRPN_NUM);
 
    if (status != 208) { // IGNORE AFTERTOUCH
       if (NOTE_ON || NOTE_OFF) { // NOTE
-         var OCTAVE = (parseInt((NOTE_NUM / 12)) - 1);
-         var FREQ   = 0 || (NOTE_NUM > 0 && NOTE_NUM < 128) ? Math.pow(2, (NOTE_NUM - 69) / 12) * 440 : null
-         var PITCH  = CHROMATIC[NOTE_NUM % 12] + OCTAVE;
-         var LETTER = PITCH.slice(0, 1);
-         println("NOTE: " + NOTE_NUM + ", OCTAVE: " + OCTAVE + ", NOTE: " + LETTER + ", PITCH: " + PITCH + ", FREQUENCY: " + parseInt(FREQ));
+         var VELOCITY = data2;
+         var OCTAVE   = (parseInt((NOTE_NUM / 12)) - 1);
+         var FREQ     = 0 || (NOTE_NUM > 0 && NOTE_NUM < 128) ? Math.pow(2, (NOTE_NUM - 69) / 12) * 440 : null
+         var PITCH    = CHROMATIC[NOTE_NUM % 12] + OCTAVE;
+         var LETTER   = PITCH.slice(0, 1);
+         println("NOTE: " + NOTE_NUM + ", VELOCITY: " + VELOCITY + ", OCTAVE: " + OCTAVE + ", NOTE: " + LETTER + ", PITCH: " + PITCH + ", FREQUENCY: " + parseInt(FREQ));
       } else if (typeof(CC_MAP[data1]) != 'undefined') { // CC
          CONTROLLER = CC_MAP[data1];
          idx = CC_IDX[CONTROLLER];
